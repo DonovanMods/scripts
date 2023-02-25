b30b678ba1b16e3c53f07606e4829eec8269e752
Donovan Young <dyoung522@gmail.com>
2023-02-25 14:51:55 -0500

    Optimize logging

--- convert_dn.sh
+++ convert_dn.sh
@@ -10,7 +10,7 @@ _convert_to_webp() {
     OUT="${BASE%_hi}.webp"
   fi
 
-  # echo "Converting $FILE to $OUT" | tee -a $LOGFILE
+  # _log "Converting $FILE to $OUT"
   cwebp -quiet -q 100 -lossless "$FILE" -o "$OUT" && rm "$FILE"
 }
 
@@ -19,18 +19,18 @@ _unzip_images() {
   local DIR="$2"
 
   if [[ -z "$ZIP" || -z "$DIR" ]]; then
-    echo "unzip_images requires a ZIP and DIR" | tee -a $LOGFILE
+    _log "unzip_images requires a ZIP and DIR"
     return 1
   fi
 
-  echo "Extracting hi-res PNGs from $ZIP to $DIR" | tee -a $LOGFILE
+  _log "Extracting hi-res PNGs from $ZIP to $DIR"
   unzip -qqojC "$ZIP" "*_hi.png" "*hi*/*.png" "*400*/*.png" -x "__MACOSX/**" -d "$DIR" 2>/dev/null
 
   if ls "$DIR"/*.png >/dev/null 2>&1; then
     return 0
   else
     # Last ditch effort if the above fails
-    echo "Failed to find any hi-res images, unzipping all PNGs" | tee -a $LOGFILE
+    _log "Failed to find any hi-res images, unzipping all PNGs"
     unzip -qqojC "$ZIP" "*.png" -x "__MACOSX/**" -d "$DIR" 2>/dev/null
 
     ls "$DIR"/*.png >/dev/null 2>&1
@@ -39,20 +39,26 @@ _unzip_images() {
   fi
 }
 
+_log() {
+  local MSG="$1"
+
+  echo "$MSG" | tee -a $LOGFILE
+}
+
 # Create and add timestamp to the $LOGFILE file
 LOGFILE="${0%.sh}.log"
 
 echo "Logging output to $LOGFILE"
-echo "Starting conversion at $(date)" | tee -a $LOGFILE
+_log "Starting conversion at $(date)"
 
 # Check for cwebp
 which cwebp >/dev/null || {
-  echo "This script requires 'cwebp' to be installed and in your \$PATH" | tee -a $LOGFILE
+  _log "This script requires 'cwebp' to be installed and in your \$PATH"
   exit 1
 }
 
 ls *.zip >/dev/null 2>&1 || {
-  echo "No ZIP files found" | tee -a $LOGFILE
+  _log "No ZIP files found"
   exit 1
 }
 
@@ -63,27 +69,32 @@ for ZIP in *.zip; do
   DIR="${ZIP%.zip}"
 
   if [[ -d "$DIR" ]]; then
-    echo "skipping $ZIP; if this is unexpected, please remove $DIR and run this again" | tee -a $LOGFILE
+    _log "skipping $ZIP; if this is unexpected, please remove $DIR and run this again"
     continue
   fi
 
   if _unzip_images "$ZIP" "$DIR"; then
-    echo "Converting files in $DIR" | tee -a $LOGFILE
+    _log "Converting files in $DIR"
     cd $DIR
     for FILE in *.png; do
       _convert_to_webp "$FILE" &
     done
     wait # for all backgrounded _convert_to_webp processes to finish
     cd ..
-    echo "Converted $(ls "$DIR"/*.webp | wc -l) images" | tee -a $LOGFILE
-    echo "Zipping $DIR" | tee -a $LOGFILE
-    zip -q -r "webp/$DIR.webp.zip" "$DIR" && unzip -tq "webp/$DIR.webp.zip" && rm -rf "$DIR" "$ZIP" || echo "Failed to Zip $DIR" | tee -a $LOGFILE
+    _log "Converted $(ls "$DIR"/*.webp | wc -l) images"
+    _log "Zipping $DIR"
+    zip -q -r "webp/$DIR.webp.zip" "$DIR" && unzip -tq "webp/$DIR.webp.zip"
+    if [[ $? -eq 0 ]]; then
+      rm -rf "$DIR" "$ZIP"
+    else
+      _log "Failed to Zip $DIR"
+    fi
   else
-    echo "Failed to Unzip $ZIP" | tee -a $LOGFILE
+    _log "Failed to Unzip $ZIP"
     [[ -d "$DIR" ]] && rm -rf "$DIR"
     continue
   fi
 done
 
-echo "Converted $(ls webp/*.zip | wc -l) ZIP files to webp" | tee -a $LOGFILE
-echo "Completed Processing at $(date)" | tee -a $LOGFILE
+_log "Converted $(ls webp/*.zip | wc -l) ZIP files to webp"
+_log "Completed Processing at $(date)"
